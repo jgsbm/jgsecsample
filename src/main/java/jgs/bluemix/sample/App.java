a9d45d6534af1f956024e5977eba2f0abdc34416
Ryozo Watanabe <clearstor@gmail.com>
2015-05-02 21:24:57 +0900

    不要コメントの除去

--- src/main/java/jgs/bluemix/sample/App.java
+++ src/main/java/jgs/bluemix/sample/App.java
@@ -3,9 +3,6 @@ package jgs.bluemix.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Created by ryozo on 2015/05/02.
- */
 @SpringBootApplication
 public class App {
 
