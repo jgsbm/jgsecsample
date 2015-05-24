e66e323745ccf328f28bd2e402ed20e56929624a
Ryozo Watanabe <clearstor@gmail.com>
2015-05-24 17:32:09 +0900

    ログイン機能を実装

---
+++ src/main/java/jgs/bluemix/sample/service/LoginUserDetailService.java
@@ -0,0 +1,37 @@
+package jgs.bluemix.sample.service;
+
+import jgs.bluemix.sample.entity.Customer;
+import jgs.bluemix.sample.entity.LoginUser;
+import jgs.bluemix.sample.repository.CustomerMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * ログインユーザ情報を取得するためのServiceクラスです.
+ * 当クラスはSpring-Securityによってログインユーザの取得servicとして利用されます.
+ */
+@Service
+public class LoginUserDetailService implements UserDetailsService {
+    @Autowired
+    CustomerMapper customerMapper;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        if (email == null) {
+            throw new IllegalArgumentException("email is null");
+        }
+        Customer customer = customerMapper.findCustomerByMail(email);
+        if (customer == null) {
+            throw new UsernameNotFoundException("User is not found");
+        }
+        return new LoginUser(customer);
+    }
+
+}
