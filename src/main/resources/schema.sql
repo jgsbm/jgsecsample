09d479d7c6695d29bc84978ee3394e9d11d45505
Ryozo Watanabe <clearstor@gmail.com>
2015-05-24 20:25:57 +0900

    Product、およびStockテーブルのDDLと初期投入データの作成

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -10,3 +10,26 @@ CREATE TABLE customer(
   update_date TIMESTAMP NOT NULL,
   version BIGINT NOT NULL
 );
+
+--create product table
+CREATE TABLE product(
+  id BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  item_code CHAR(10) NOT NULL UNIQUE,
+  item_name VARCHAR(50),
+  price DECIMAL(6, 2),
+  pic BLOB,
+  detail VARCHAR(200),
+  create_date TIMESTAMP NOT NULL,
+  update_date TIMESTAMP NOT NULL,
+  version BIGINT NOT NULL
+);
+
+--create stock
+CREATE TABLE stock(
+  id BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  stock INT NOT NULL,
+  product_id BIGINT NOT NULL UNIQUE,
+  create_date TIMESTAMP NOT NULL,
+  update_date TIMESTAMP NOT NULL,
+  version BIGINT NOT NULL
+);
