7a5feaee5f4e85fbeb1854b931fe421dc1532ed2
Ryozo Watanabe <clearstor@gmail.com>
2015-05-24 16:39:11 +0900

    CustomerテーブルのDDL、およびアクセス用のMapperを追加

---
+++ src/main/java/jgs/bluemix/sample/entity/BaseEntity.java
@@ -0,0 +1,21 @@
+package jgs.bluemix.sample.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+/**
+ * 全てのエンティティのベースクラスです.
+ * 全エンティティ共通のフィールドを保持します。
+ *
+ * @author ryozo
+ */
+@Data
+public abstract class BaseEntity implements Serializable {
+    private static final long serialVersionUID = 5776265570323589855L;
+    private Long id;
+    private Timestamp createDate;
+    private Timestamp updateDate;
+    private Long version;
+}
