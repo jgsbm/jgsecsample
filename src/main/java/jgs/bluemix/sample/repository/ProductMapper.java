7ab48ff6427ea27ef1c79619ece8f7a7f6977dd6
Ryozo Watanabe <clearstor@gmail.com>
2015-05-31 19:05:15 +0900

    Blobを取り扱うSQLを追加

--- src/main/java/jgs/bluemix/sample/repository/ProductMapper.java
+++ src/main/java/jgs/bluemix/sample/repository/ProductMapper.java
@@ -1,6 +1,7 @@
 package jgs.bluemix.sample.repository;
 
 import jgs.bluemix.sample.entity.Product;
+import jgs.bluemix.sample.entity.ProductPic;
 
 import java.util.List;
 
@@ -16,5 +17,13 @@ public interface ProductMapper {
      * Productの一覧はProductCodeの昇順で返却します。
      */
     List<Product> findAllStockProducts();
+
+    /**
+     * 指定された商品コード(ItemCode)を保持する商品の画像(PIC)を保持する{@link ProductPic}インスタンスを返却します.
+     *
+     * @param itemCode 検索対象商品の商品コード
+     * @return 商品画像を保持するProductPicインスタンス
+     */
+    ProductPic findProductPic(String itemCode);
 
 }
