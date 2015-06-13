dd5feb9d1e20e9222fe792b57b0c04b7672d904e
Ryozo Watanabe <clearstor@gmail.com>
2015-06-13 18:48:57 +0900

    ProductNotFoundExceptionを復帰不能例外に指定

--- src/main/java/jgs/bluemix/sample/exception/ProductNotFoundException.java
+++ src/main/java/jgs/bluemix/sample/exception/ProductNotFoundException.java
@@ -3,11 +3,11 @@ package jgs.bluemix.sample.exception;
 import jgs.bluemix.sample.message.MessageCodeEnum;
 
 /**
- * 商品が存在しないことによって何らかの業務操作が実施できない場合に発生する業務例外です.
+ * 商品が存在しないことによって何らかの業務操作が実施できない場合に発生する復帰不能例外です.
  *
  * @author ryozo
  */
-public class ProductNotFoundException extends BusinessException {
+public class ProductNotFoundException extends SystemException {
 
     private static final long serialVersionUID = -2967719464584428634L;
 
